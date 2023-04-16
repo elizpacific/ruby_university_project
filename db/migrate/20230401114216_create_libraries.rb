46f1dd4aa3ff76b83491019a4527db9d006a7eb8
elizpacific <elizpacific@gmail.com>
2023-04-16 16:17:28 +0300

    insert data by csv file

--- db/migrate/20230401114216_create_libraries.rb
+++ db/migrate/20230401114216_create_libraries.rb
@@ -3,7 +3,7 @@ class CreateLibraries < ActiveRecord::Migration[7.0]
     create_table :libraries do |t|
       t.string :name
       t.string :address
-      t.integer :number
+      t.string :library_id
 
       t.timestamps
     end
